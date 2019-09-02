dc95ee9b353aba23c4f0d30e91ecb5d884e509f1
mli <mli@student.42.fr>
2019-09-02 20:06:29 +0200

    combn on avance doucement mais surement

--- ft_print_combn.c
+++ ft_print_combn.c
@@ -6,13 +6,22 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 15:40:54 by mli               #+#    #+#             */
-/*   Updated: 2019/09/01 23:45:50 by mli              ###   ########.fr       */
+/*   Updated: 2019/09/02 20:05:59 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 #include <stdlib.h>
+
+void	ft_initialize(int *tab, int n)
+{
+	int i;
+
+	i = 0;
+	while (--n >= 0)
+		tab[n] = (i++);
+}
 
 void	ft_print(int *tab, int n)
 {
@@ -25,20 +34,52 @@ void	ft_print(int *tab, int n)
 	}
 }
 
+int		ft_check(int *tab, int n)
+{
+	while (--n > 0)
+		if (tab[n] >= tab[n - 1])
+			return (0);
+	return (1);
+}
+
 void	ft_print_combn(int n)
 {
 	int i;
+	int k;
 	int tab[n];
 
 	i = 0;
-	tab[i] = n - 1;
-//	while (tab[n - 1] <= (10 - n))
-//	{
-		i = -1;
-		while (++i <= n - 2)
-			tab[i + 1] = (tab[i] - 1);
-		ft_print(tab, n);
-//	}
+	k = 0;
+	ft_initialize(tab, n);
+	while (tab[n - 1] <= (10 - n))
+	{
+		while (tab[i] <= (9 - i) && (i < n - 1))
+		{
+			if (ft_check(tab, n))
+			{
+				ft_print(tab, n);
+				if (tab[n - 1] < (10 - n))
+					write(1, ", ", 2);
+			}
+			if (tab[i] == (9 - i) && (i < n - 1))
+			{
+				tab[i + 1]++;
+				tab[i] = tab[i + 1] + 1;
+			}
+			// Then
+			k = 0;
+			while ((tab[i + k] == (9 - i)) && (i + k < n - 1))
+				k++;
+			// On a k tq => tab[i + k] != max
+			while (k >= 0)
+			{
+				tab[i + k]++;
+			}
+			if (tab[i] != (9 - i) && (i < n - 1))
+				tab[i]++;
+		}
+		break;
+	}
 
 	write(1, "\nEnd\n", 5);
 }
