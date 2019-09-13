3f45ecc3a47dfc74cb25b4f012b84e59360fdf36
mli <mli@student.42.fr>
2019-09-13 23:30:58 +0200

    convert base avancement

--- ft_convert_base.c
+++ ft_convert_base.c
@@ -6,8 +6,95 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 23:33:36 by mli               #+#    #+#             */
-/*   Updated: 2019/09/12 23:33:38 by mli              ###   ########.fr       */
+/*   Updated: 2019/09/13 21:29:22 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+#include <unistd.h>
+#include <stdio.h>
+
+int		ft_atoi_base(char *str, char *base, int base_len);
+void    ft_putnbr(int nbr, char *base, long int base_len, char *result);
+
+int		base_check(char *base)
+{
+	int i;
+	int j;
+
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == '\t' ||
+				base[i] == '\n' || base[i] == '\v' || base[i] == '\f' ||
+				base[i] == '\r' || base[i] == ' ')
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (base[i + 1])
+	{
+		j = i + 1;
+		while (base[j])
+			if (base[i] == base[j++])
+				return (0);
+		i++;
+	}
+	return (i + 1);
+}
+
+int		result_size(int nb, int base_len)
+{
+	int i;
+
+	i = 1;
+	if (nb < 0)
+	{
+		nb *= (-1);
+		i++;
+	}
+	while (nb >= base_len)
+	{
+		nb /= base_len;
+		i++;
+	}
+	return (i + 1);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	long int	nb;
+	int			base_len_from;
+	int			base_len_to;
+	char		*result;
+	int			size;
+
+	base_len_from = base_check(base_from);
+	base_len_to = base_check(base_to);
+	if (!(base_len_from) || !(base_len_to))
+		return (NULL);
+
+	nb = (long int)ft_atoi_base(nbr, base_from, base_len_from);
+	size = result_size(nb, base_len_to);
+	if (!(result = (char *)malloc(sizeof(char) * size)))
+		return (NULL);
+	printf("Result size: %d\n", result_size(nb, base_len_to) - 1);
+	ft_putnbr(nb, base_to, base_len_to, result);
+	result[size - 1] = '\0';
+	free(result);
+	return(result);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("%s\n", ft_convert_base(argv[1], "0123456789", "0123456789ABCDEF"));
+	else if (argc == 3)
+		printf("%s\n", ft_convert_base(argv[1], argv[2], "0123456789ABCDEF"));
+	else if (argc == 4)
+		printf("%s\n", ft_convert_base(argv[1], argv[2], argv[3]));
+	return (0);
+}
--- ft_convert_base2.c
+++ ft_convert_base2.c
@@ -6,8 +6,88 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 23:33:41 by mli               #+#    #+#             */
-/*   Updated: 2019/09/12 23:33:45 by mli              ###   ########.fr       */
+/*   Updated: 2019/09/13 21:51:43 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+#include <unistd.h>
+#include <stdio.h>
+
+int		in_base(char to_test, char *base)
+{
+	int i;
+
+	i = -1;
+	while (base[++i])
+		if (base[i] == to_test)
+			return (i);
+	return (0);
+}
+
+int		power(int x, int puissance)
+{
+	int i;
+	int r;
+
+	i = 0;
+	r = 1;
+	while (i++ < puissance)
+		r *= x;
+	return (r);
+}
+
+int		ft_atoi_base(char *str, char *base, int base_len)
+{
+	int i;
+	int signe;
+	int result;
+	int str_max;
+
+	i = 0;
+	signe = 1;
+	result = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			signe *= (-1);
+	str_max = i;
+	while (in_base(str[str_max], base) || (base[0] == str[str_max]))
+		str_max++;
+	str_max--;
+	while ((in_base(str[i], base) || base[0] == str[i]) && (i <= str_max))
+	{
+		result += (in_base(str[i], base) * power(base_len, (str_max - i)));
+		i++;
+	}
+	return (result * signe);
+}
+
+void    ft_convert(long int nbr, char *base, long int base_len, char *r, int i)
+{
+    if (nbr >= base_len)
+    {
+        ft_convert((nbr / base_len), base, base_len, r, i++);
+        ft_convert((nbr % base_len), base, base_len, r, i);
+    }
+    else
+		r[i] = base[nbr];
+}
+
+void    ft_putnbr(int nbr, char *base, long int base_len, char *result)
+{
+    long int nb;
+
+    nb = (long int)nbr;
+	if (nbr < 0)
+	{
+		result[0] = '-';
+		nb = nbr * (-1);
+		ft_convert(nb, base, base_len, result, 1);
+	}
+	else
+		ft_convert(nb, base, base_len, result, 0);
+}
