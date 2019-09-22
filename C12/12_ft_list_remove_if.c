b3567b89c7bc52d45e5947ab700e6f0e2790ce8b
mli <mli@student.42.fr>
2019-09-22 13:24:25 +0200

    list remove if finished + main test done

--- C12/12_ft_list_remove_if.c
+++ C12/12_ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 22:59:47 by mli               #+#    #+#             */
-/*   Updated: 2019/09/21 23:54:10 by mli              ###   ########.fr       */
+/*   Updated: 2019/09/22 13:12:10 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
 	t_list	*current;
 	t_list	*to_delete;
 
-	while (!((*cmp)((*begin_list)->data, data_ref)) && *begin_list)
+	while (*begin_list && !((*cmp)((*begin_list)->data, data_ref)))
 	{
 		to_delete = *begin_list;
 		*begin_list = to_delete->next;
@@ -28,14 +28,14 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
 	current = *begin_list;
 	while (current != NULL)
 	{
-		if (!((*cmp)(current->next->data, data_ref)) && current->next)
+		if (current->next && !((*cmp)(current->next->data, data_ref)))
 		{
 			to_delete = current->next;
 			current->next = current->next->next;
 			(*free_fct)(to_delete->data);
 			free(to_delete);
 		}
-		while ((*cmp)(current->data, data_ref) && current)
+		else
 			current = current->next;
 	}
 }
--- C12/main_test.c
+++ C12/main_test.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 23:10:40 by mli               #+#    #+#             */
-/*   Updated: 2019/09/19 22:32:20 by mli              ###   ########.fr       */
+/*   Updated: 2019/09/22 13:16:30 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,28 @@
 t_list	*ft_list_push_strs(int size, char **strs);
 void	ft_list_reverse(t_list **begin_list);
 
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
+		void (*free_fct)(void *));
+
+int		compare(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (0);
+	return (1);
+}
+
+void	free_function(void *data)
+{
+	printf("Deleted: %s\n", data);
+}
+
 void	ft_print_list(t_list *begin_list)
 {
 	while (begin_list != NULL)
 	{
 		printf("%s", begin_list->data);
 			if (begin_list->next)
-				printf(" || ");
+				printf(" -> ");
 		begin_list = begin_list->next;
 	}
 	printf("\n");
@@ -32,11 +47,21 @@ void	ft_print_list(t_list *begin_list)
 int		main(void)
 {
 	t_list	*list;
-
-	char *strs[] = {"Hello", "Moon", "Sun", "Hey", "Wouah"};
+	char *strs[] = {"1", "2", "3", "4", "R"};
+//	00 create elem + 05 push strs
 	list = ft_list_push_strs(5, strs);
 	ft_print_list(list);
+//	08 List Reverse
 	ft_list_reverse(&list);
 	ft_print_list(list);
+//	12 Remove if
+	printf("\n");
+	int (*cmp)() = &compare;
+	void (*free_fct)(void *) = &free_function;
+	ft_list_remove_if(&list, "R", cmp, free_fct);
+	printf("\nList Content:\n");
+	ft_print_list(list);
+
+
 	return (0);
 }
